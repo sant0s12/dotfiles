399f8d6cfe94a99553c5f6cc7d375e9257116222
sant0s12 <diego.delossantos@mailbox.org>
2024-05-10 16:24:27 +0200

    NixOS gen. 257 - 2024-05-10 16:24:23 24.05.20240509.f1010e0 6.6.30

--- nixos/.config/nixos/nixos/configuration.nix
+++ nixos/.config/nixos/nixos/configuration.nix
@@ -147,7 +147,7 @@
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
-    # package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
 
   xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
